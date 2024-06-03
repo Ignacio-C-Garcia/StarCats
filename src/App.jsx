a6ab9298f4fad02c8a2caedda76ed16e7649620e
Ichiro The Cringy <agustingabrielgarcia@outlook.com>
2024-06-03 19:01:08 -0300

    pages empty pages and module react-router-dom installed (npm i)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-
 import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
