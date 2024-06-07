5cb2c7aaca5bc5ea7d9f6fbf16da65727f64f52c
Ichiro The Cringy <agustingabrielgarcia@outlook.com>
2024-06-07 18:15:14 -0300

    page icon fixed, login css and jsx modified

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,51 +13,56 @@ const Login = () => {
   };
 
   return (
-    <Container className="vh-100 d-flex justify-content-center">
-      <Row>
-        <Col
-          xs={12}
-          md={12}
-          lg={6}
-          className="m-0 p-3 d-flex justify-content-center"
-        >
-          <img src="/logostarcat.svg" alt="starcat-logo" />
-        </Col>
-        <Col className="m-0 p-3 d-flex align-items-center">
-          <form
-            onSubmit={handleSubmit}
-            className="border rounded-4 p-4 d-flex align-items-center w-100"
+    <div className="login">
+      <Container className="vh-100 d-flex justify-content-center">
+        <Row>
+          <Col
+            xs={12}
+            md={12}
+            lg={6}
+            className="m-0 p-3 d-flex justify-content-center"
           >
-            <div className="w-100">
-              <h2 className="text-center mb-3">Iniciar sesión</h2>
-              <div>
-                <input
-                  className="mb-2 p-2 border rounded-pill w-100 text-center"
-                  placeholder="Correo electrónico"
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
+            <img src="/logostarcats.svg" alt="starcat-logo" />
+          </Col>
+          <Col className="m-0 p-3 d-flex  flex-wrap align-items-center">
+            <h2 className="text-center mb-3">Iniciar sesión</h2>
+            <form
+              onSubmit={handleSubmit}
+              className="rounded-4 p-4 d-flex align-items-center w-100"
+            >
+              <div className="w-100">
+                <div>
+                  <input
+                    className="mb-2 p-2 border-0 rounded-pill w-100 text-center"
+                    placeholder="Correo electrónico"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+                <div>
+                  <input
+                    className="mb-2 p-2 border-0 rounded-pill w-100 text-center"
+                    placeholder="Contraseña"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="p-2 border-0 rounded-pill w-100"
+                >
+                  Entrar
+                </button>
               </div>
-              <div>
-                <input
-                  className="mb-2 p-2 border rounded-pill w-100 text-center"
-                  placeholder="Contraseña"
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
-              </div>
-              <button type="submit" className="p-2 border rounded-pill w-100">
-                Entrar
-              </button>
-            </div>
-          </form>
-        </Col>
-      </Row>
-    </Container>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
