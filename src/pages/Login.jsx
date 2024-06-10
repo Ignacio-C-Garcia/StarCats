fa43bb2ae9855904915b7871f5eb7786d7598b6f
Ichiro The Cringy <130695643+ichirothecringy-dev@users.noreply.github.com>
2024-06-10 19:06:03 -0300

    changes in app and login css, changes in login and register jsx, modules updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import "../Login.css";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
+import Footer from "../components/Footer";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -14,7 +15,7 @@ const Login = () => {
   };
 
   return (
-    <div className="login">
+    <div className="main">
       <Container className="vh-100 d-flex justify-content-center">
         <Row>
           <Col
@@ -23,11 +24,17 @@ const Login = () => {
             lg={{ span: 6, order: "first" }}
             className="m-0 p-3 d-flex justify-content-center"
           >
-            <img src="/logostarcats.svg" alt="starcat-logo" />
+            <a href="/" className="d-flex justify-content-center">
+              <img src="/logostarcats.svg" alt="starcat-logo" />
+            </a>
           </Col>
           <Col className="m-0 p-3 d-flex flex-column justify-content-center">
             <h2 className="text-center mb-3 fs-1">Iniciar sesión</h2>
             <div className="form rounded-4 p-4 w-100">
+              -- Editar el mensaje de advertencia al no llenar los campos --{" "}
+              <br />
+              -- Crear formulario (posiblemente sera la configuracion de
+              usuario) de cambio de contraseña --
               <form
                 onSubmit={handleSubmit}
                 className="d-flex align-items-center pb-2"
@@ -61,30 +68,23 @@ const Login = () => {
                   </button>
                 </div>
               </form>
-              <div className="d-flex flex-row gap-1 w-100">
-                <Link to="/signup">
-                  <button className="p-2 border-0 rounded-pill">
-                    No tengo cuenta
+              <div class="d-flex gap-1">
+                <Link to="/signup" class="flex-fill">
+                  <button class="p-2 border-0 rounded-pill w-100">
+                    Registrarse
                   </button>
                 </Link>
-                <Link to="/home">
-                  <button className="p-2 border-0 rounded-pill">
-                    Solo quiero ver
+                <Link to="" class="flex-fill">
+                  <button class="p-2 border-0 rounded-pill w-100">
+                    ¿Olvidaste la contraseña?
                   </button>
                 </Link>
               </div>
             </div>
-            <p>
-              <a
-                className="link-offset-2 link-underline link-underline-opacity-0"
-                href="/signup"
-              >
-                Me olvide de mi contraseña T.T
-              </a>
-            </p>
           </Col>
         </Row>
       </Container>
+      <Footer />
     </div>
   );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import "../Login.css";
 import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import Footer from "../components/Footer";
 
 const Register = () => {
   const [email, setEmail] = useState("");
@@ -12,7 +13,7 @@ const Register = () => {
   };
 
   return (
-    <div className="login">
+    <div className="main">
       <Container className="vh-100 d-flex justify-content-center">
         <Row>
           <Col
@@ -21,17 +22,19 @@ const Register = () => {
             lg={{ span: 6, order: "first" }}
             className="m-0 p-3 d-flex justify-content-center"
           >
-            <img src="/logostarcats.svg" alt="starcat-logo" />
+            <a href="/" className="d-flex justify-content-center">
+              <img src="/logostarcats.svg" alt="starcat-logo" />
+            </a>
           </Col>
           <Col className="m-0 p-3 d-flex flex-column justify-content-center">
             <h2 className="text-center mb-3 fs-1">Regístrate</h2>
             <form
               onSubmit={handleSubmit}
-              className="rounded-4 p-4 d-flex align-items-center w-100"
+              className="form rounded-4 p-4 d-flex align-items-center w-100"
             >
               <div className="w-100">
-                // volver en el futuro para editar los valores que correspondan
-                al back
+                -- Volver en el futuro para editar los valores que correspondan
+                al back --
                 <div>
                   <input
                     className="mb-2 p-2 border-0 rounded-pill w-100 text-center"
@@ -73,6 +76,7 @@ const Register = () => {
           </Col>
         </Row>
       </Container>
+      <Footer />
     </div>
   );
 };
