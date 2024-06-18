f277d7554912a1047df87786b8f5c9f342f0b491
ignacio <ignacioacgarcia@gmail.com>
2024-06-18 15:58:21 -0300

    productos y gatos extraidos de la api

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,12 +4,18 @@ function ProductCard({ product }) {
   return (
     <div className={`card ${styles.cardCss}`}>
       <div className={styles["img-container"]}>
-        <img src={product.image} alt={product.alt} className={styles.img} />
+        <img
+          src={import.meta.env.VITE_IMG_PATH + product.pic}
+          //     alt={product.alt}
+          className={styles.img}
+        />
       </div>
       <div>
         <div className={styles.cardContent}>
           <h5 className="card-title">{product.name}</h5>
-          <small className="text-muted m-2 fs-4 fw-bold">${product.price}</small>
+          <small className="text-muted m-2 fs-4 fw-bold">
+            ${product.price}
+          </small>
           <button type="button" className={` ${styles.btn}`}>
             Agregar al carrito
           </button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,44 @@ import styles from "../styles/Home.module.css";
 import CarouselCats from "../components/CarouselCats";
 import CarouselProducts from "../components/CarouselProducts";
 import Footer from "../components/Footer";
-
+import { useEffect, useState } from "react";
 function Home() {
+  const [products, setProducts] = useState([]);
+  const [cats, setCats] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          import.meta.env.VITE_API_URL + "/products"
+        );
+        if (!response.ok) {
+          throw new Error("API fetch error, !ok");
+        }
+        const data = await response.json();
+        setProducts(data.products);
+      } catch (error) {
+        throw new Error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(import.meta.env.VITE_API_URL + "/kittens");
+        if (!response.ok) {
+          throw new Error("API fetch error, !ok");
+        }
+        const data = await response.json();
+        setCats(data.kittens);
+      } catch (error) {
+        throw new Error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <>
       <header>
@@ -81,8 +117,8 @@ function Home() {
         </div>
       </section>
 
-      <CarouselProducts />
-      <CarouselCats />
+      <CarouselProducts products={products} />
+      <CarouselCats cats={cats} />
       <Footer />
     </>
   );
