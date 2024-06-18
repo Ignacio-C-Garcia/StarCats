ddd7f55ac6aefbc2940437b5daffe4d74fccaab6
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2024-06-18 20:03:34 -0300

    product list with api fetch

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,15 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import data from "./coffee.json";
+
 import ProductCard from "./ProductCard";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          import.meta.env.VITE_API_URL + "/products"
+        );
+        if (!response.ok) {
+          throw new Error("API fetch error, !ok");
+        }
+        const data = await response.json();
+        setProducts(data.products);
+      } catch (error) {
+        throw new Error(error);
+      }
+    };
 
+    fetchData();
+  }, []);
   return (
     <Container>
       <Row>
-        {data.map((product) => (
+        {products.map((product) => (
           <div key={product.id} className="col col-xs-6 col-md-6 col-lg-4 mb-4">
             <ProductCard product={product} />
           </div>
