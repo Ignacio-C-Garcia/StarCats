a18520297457db08c8ada29432a844cdce840766
Jean Franco <jeansueiro60@gmail.com>
2024-06-26 21:25:11 -0300

    fix: 🐞

--- src/components/ProductModal.jsx
+++ src/components/ProductModal.jsx
@@ -1,78 +1,155 @@
 import React, { useState } from "react";
 import { Modal as BootstrapModal, Button, Form } from "react-bootstrap";
-import styles from "../styles/ProductModal.module.css"; // Incluye estilos personalizados
+import styles from "../styles/ProductModal.module.css";
 
 function ProductModal({ show, setShow, product }) {
   const handleClose = () => setShow(false);
 
-  const [size, setSize] = useState("mediano");
   const [isToGo, setIsToGo] = useState("aqui");
   const [espressoShots, setEspressoShots] = useState(1);
   const [quantity, setQuantity] = useState(1);
   const [volume, setVolume] = useState(250);
 
-  const handleSizeSelection = (selectedSize) => {
-    setSize(selectedSize);
+  const handleVolumeChange = (vol) => {
+    setVolume(vol);
+  };
+
+  const handleIsToGoChange = (option) => {
+    setIsToGo(option);
   };
 
   const handleAddToCart = () => {
-    // Lógica para añadir el producto al carrito
     console.log({
       product,
-      size,
+      size: "mediano",
       isToGo,
       espressoShots,
       quantity,
-      volume
+      volume,
     });
     setShow(false);
   };
 
+  const incrementQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decrementQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
   return (
-    <BootstrapModal show={show} onHide={handleClose} dialogClassName={styles.customModal}>
+    <BootstrapModal
+      show={show}
+      onHide={handleClose}
+      dialogClassName={styles.customModal}
+    >
       <BootstrapModal.Body>
-        <div className="row">
-          <div className="col-12 text-center mb-4">
-            <h2>{product.name}</h2>
-          </div>
-          <div className="col-6">
-            <img
-              src={`${import.meta.env.VITE_IMG_PATH}${product.pic}`}
-              alt={product.alt}
-              className={`img-fluid ${styles.productImage}`}
-            />
-          </div>
-          <div className="col-6">
-            <p>{product.description}</p>
-            <p>
-              <strong>Precio: </strong>${product.price}
-            </p>
-          
-            <Form>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12 col-md-6 mb-4">
+              <img
+                src={`${import.meta.env.VITE_IMG_PATH}${product.pic}`}
+                alt={product.alt}
+                className={` ${styles.productImage}`}
+              />
+              <p>{product.description}</p>
+            </div>
+            <div className="col-12 col-md-6 mt-4">
+              <h3>{product.name}</h3>
               <Form.Group className="mb-3">
-                <Form.Label>Onsite / Takeaway</Form.Label>
-                <Form.Control as="select" value={isToGo} onChange={(e) => setIsToGo(e.target.value)}>
-                  <option value="aqui">Onsite</option>
-                  <option value="llevar">Takeaway</option>
-                </Form.Control>
+                <div className="d-flex align-items-center">
+                  <Button
+                    variant="outline-primary"
+                    className="me-2"
+                    onClick={decrementQuantity}
+                  >
+                    -
+                  </Button>
+                  <div className="d-flex align-items-center">
+                    <Form.Control
+                      type="text"
+                      value={quantity}
+                      onChange={(e) => {
+                        const value = e.target.value;
+                        setQuantity(value);
+                      }}
+                      style={{ width: "50px", textAlign: "center" }}
+                    />
+                    <Button
+                      variant="outline-primary"
+                      className="ms-2"
+                      onClick={incrementQuantity}
+                    >
+                      +
+                    </Button>
+                  </div>
+                </div>
               </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Volume, ml</Form.Label>
-                <Form.Control as="select" value={volume} onChange={(e) => setVolume(e.target.value)}>
-                  <option value="250">250 ml</option>
-                  <option value="350">350 ml</option>
-                  <option value="450">450 ml</option>
-                </Form.Control>
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Cantidad de espressos</Form.Label>
-                <Form.Control type="number" value={espressoShots} onChange={(e) => setEspressoShots(e.target.value)} min="1" max="10" />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Cantidad de productos</Form.Label>
-                <Form.Control type="number" value={quantity} onChange={(e) => setQuantity(e.target.value)} min="1" max="10" />
-              </Form.Group>
-            </Form>
+              <Form>
+                <Form.Group className="mb-3">
+                  <div className="row">
+                    <div className="col-6 mb-3">
+                      <p>Onsite</p>
+                      <i
+                        className={`bi bi-cup-straw fs-2  ${
+                          isToGo === "aqui" ? styles.selectedIcon : ""
+                        } ${styles.hoverIcon}`}
+                        onClick={() => handleIsToGoChange("aqui")}
+                      ></i>
+                    </div>
+                    <div className="col-6">
+                      <p>Takeaway</p>
+                      <i
+                        className={`bi bi-bag fs-2 ${
+                          isToGo === "llevar" ? styles.selectedIcon : ""
+                        } ${styles.hoverIcon}`}
+                        onClick={() => handleIsToGoChange("llevar")}
+                      ></i>
+                    </div>
+                  </div>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <p>Tamaño</p>
+                  <div className={styles.volumeSelector}>
+                    <div className={styles.volumeOption}>
+                      <i
+                        className={`bi bi-cup-hot fs-5 ${
+                          volume === 250 ? styles.selectedIcon : ""
+                        } ${styles.hoverIcon}`}
+                        onClick={() => handleVolumeChange(250)}
+                      ></i>
+                      <span>250 ml</span>
+                    </div>
+                    <div className={styles.volumeOption}>
+                      <i
+                        className={`bi bi-cup-hot fs-4  ${
+                          volume === 350 ? styles.selectedIcon : ""
+                        } ${styles.hoverIcon}`}
+                        onClick={() => handleVolumeChange(350)}
+                      ></i>
+                      <span>350 ml</span>
+                    </div>
+                    <div className={styles.volumeOption}>
+                      <i
+                        className={`bi bi-cup-hot fs-3 ${
+                          volume === 450 ? styles.selectedIcon : ""
+                        } ${styles.hoverIcon}`}
+                        onClick={() => handleVolumeChange(450)}
+                      ></i>
+                      <span>450 ml</span>
+                    </div>
+                  </div>
+                </Form.Group>
+
+                <p>
+                  <strong>Precio: </strong>${product.price}
+                </p>
+              </Form>
+              <div className="text-end"></div>
+            </div>
           </div>
         </div>
       </BootstrapModal.Body>
