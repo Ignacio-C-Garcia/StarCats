54cb5b7e420215f8276a9bf1146bc33aa19e1096
ignacio <ignacioacgarcia@gmail.com>
2024-07-02 16:43:13 -0300

    agregado estado global shoppingcart

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import App from "./App.jsx";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import authReducer from "./redux/authReducer.jsx";
+import ShoppingCartReducer from "./redux/shoppingCartReducer.jsx";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    shoppingCart: ShoppingCartReducer,
   },
 });
 
