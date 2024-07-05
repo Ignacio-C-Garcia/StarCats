31dbb4475d5078a9991381ecac3d3deb7c7e9c5c
Jean <jeansueiro60@gmail.com>
2024-07-05 20:54:40 -0300

    page not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,21 @@
+import styles from "../styles/NotFound.module.css";
+
 function NotFound() {
-  return <div>NotFound dlscklms  </div>;
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <h3 className={styles.notFoundText}>Error 404</h3>
+          <button className={styles.btnHome}>Volver al inicio</button>
+        </div>
+        <img
+          src="spilledCoffee.png"
+          alt="Not Found"
+          className={styles.spilledCoffee}
+        />
+      </div>
+    </>
+  );
 }
 
 export default NotFound;
