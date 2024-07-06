62c8adba631057738a74bab3ef68d0b515cf3e99
Jean <jeansueiro60@gmail.com>
2024-07-06 01:10:23 -0300

    implemented efect parralax and page not found finished

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,18 +1,26 @@
+import { Link } from "react-router-dom";
 import styles from "../styles/NotFound.module.css";
 
 function NotFound() {
   return (
     <>
-      <div className={styles.container}>
-        <div className={styles.content}>
-          <h3 className={styles.notFoundText}>Error 404</h3>
-          <button className={styles.btnHome}>Volver al inicio</button>
+      <div className={`container-fluid   ${styles.container}`}>
+        <div className="row">
+          <div className={`col-lg-6 col-md-12  ${styles.content}`}>
+            <h3 className={` ${styles.notFoundText}`}>Error 404</h3>
+
+            <Link to="/" className={styles.btnHome}>
+              Volver al inicio
+            </Link>
+          </div>
+          <div className="col-lg-6 col-md-12">
+            <img
+              src="spilledCoffee.png"
+              alt="Not Found"
+              className={` animate__animated animate__bounce ${styles.spilledCoffee}`}
+            />
+          </div>
         </div>
-        <img
-          src="spilledCoffee.png"
-          alt="Not Found"
-          className={styles.spilledCoffee}
-        />
       </div>
     </>
   );
