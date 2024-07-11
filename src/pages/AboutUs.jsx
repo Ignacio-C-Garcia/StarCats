862fe0c5e4b714b3c818ecc228b74cf959fe8b54
Ichiro <130695643+ichirothecringy-dev@users.noreply.github.com>
2024-07-11 20:09:09 -0300

    about styles and other details partially done (fix project redaction)

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -17,10 +17,18 @@ function AboutUs() {
                 Sobre este Proyecto
               </h2>
               <p className={styles["banner-p"]}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
+                Este es el resultado de nuestro trayecto en Ánima FINEST.
+                Formamos parte de un intenso programa de formación en desarrollo
+                web, que incluye más de 200 horas prácticas distribuidas a lo
+                largo de 4 meses. Durante este tiempo, hemos aplicado nuestros
+                conocimientos y habilidades para dar vida a este proyecto.
+                <br />
+                <br />
+                Dedicamos gran parte de este semestre a desarrollar este
+                e-commerce con el objetivo de poner en practica lo aprendido.
+                Cada aspecto del diseño y la funcionalidad de nuestra aplicación
+                ha sido cuidadosamente elaborado para ofrecer a los usuarios una
+                plataforma atractiva y eficiente.
               </p>
             </div>
           </div>
@@ -30,208 +38,224 @@ function AboutUs() {
       <div className={`${styles.techBackground} container-fluid pb-3 pt-5`}>
         <Container>
           <div className="row text-center">
-            <div className="col-lg-6 m-auto">
-              <p className="pb-3">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod Lorem ipsum dolor sit amet.
-              </p>
-              <Row>
-                <Col className="my-4">
-                  <h4>Front-end</h4>
-                  <div>
-                    <div className="row d-flex gap-3 justify-content-center">
-                      <img
-                        className={styles["tech-icons"]}
-                        src="/icons/html.svg"
-                        alt="HTML"
-                      />
-                      <img
-                        className={styles["tech-icons"]}
-                        src="/icons/css.svg"
-                        alt="CSS"
-                      />
-                      <img
-                        className={styles["tech-icons"]}
-                        src="/icons/bs.svg"
-                        alt="Bootstrap"
-                      />
-                      <img
-                        className={styles["tech-icons"]}
-                        src="/icons/js.svg"
-                        alt="JavaScript"
-                      />
-                      <img
-                        className={styles["tech-icons"]}
-                        src="/icons/react.svg"
-                        alt="React"
-                      />
-                      <img
-                        className={styles["tech-icons"]}
-                        src="/icons/paypal-api.svg"
-                        alt="PayPal API"
-                      />
-                      <img
-                        className={styles["tech-icons"]}
-                        src="/icons/vite.svg"
-                        alt="Vite"
-                      />
-                      <img
-                        className={styles["tech-icons"]}
-                        src="/icons/redux.svg"
-                        alt="Redux"
-                      />
-                      <img
-                        className={styles["tech-icons"]}
-                        src="/icons/react-stripe.svg"
-                        alt="React Stripe"
-                      />
-                      <img
-                        className={styles["tech-icons"]}
-                        src="/icons/react-tabulator.png"
-                        alt="React Tabulator"
-                      />
-                    </div>
-                  </div>
-                </Col>
-                <Col className="my-4">
-                  <h4>Back-end</h4>
+            <p className="pb-3">
+              A lo largo del proyecto, hemos utilizado una amplia gama de
+              tecnologías que abarcaron tanto en el desarrollo del backend como
+              del frontend. Además hemos adquirido experiencia práctica con el
+              uso de tecnologías para nuestra base de datos.
+              <br />
+              <br />
+              Como plataforma de colaboración y control de versiones, usamos
+              Github
+              <br />
+              <br />
+              Utilizamos Trello y Meets, plataformas cruciales para facilitar
+              una comunicación fluida y para la planificación y organización de
+              todo el proyecto.
+              <br />
+              <br />
+              Estas son algunas de las herramientas que hemos elegido
+              cuidadosamente para aumentar nuestra productividad:
+            </p>
+            <Row>
+              <Col md={12} lg={6} className="my-4">
+                <h4>Front-end</h4>
+                <div>
                   <div className="row d-flex gap-3 justify-content-center">
                     <img
                       className={styles["tech-icons"]}
-                      src="/icons/express-js.svg"
-                      alt="Express"
+                      src="/icons/html.svg"
+                      alt="HTML"
                     />
                     <img
                       className={styles["tech-icons"]}
-                      src="/icons/faker-js.svg"
-                      alt="Faker.js"
+                      src="/icons/css.svg"
+                      alt="CSS"
                     />
                     <img
                       className={styles["tech-icons"]}
-                      src="/icons/node.svg"
-                      alt="Node.js"
+                      src="/icons/bs.svg"
+                      alt="Bootstrap"
                     />
                     <img
                       className={styles["tech-icons"]}
-                      src="/icons/postman.svg"
-                      alt="Postman"
+                      src="/icons/js.svg"
+                      alt="JavaScript"
                     />
+                    <img
+                      className={styles["tech-icons"]}
+                      src="/icons/react.svg"
+                      alt="React"
+                    />
+                    <img
+                      className={styles["tech-icons"]}
+                      src="/icons/paypal-api.svg"
+                      alt="PayPal API"
+                    />
+                    <img
+                      className={styles["tech-icons"]}
+                      src="/icons/vite.svg"
+                      alt="Vite"
+                    />
+                    <img
+                      className={styles["tech-icons"]}
+                      src="/icons/redux.svg"
+                      alt="Redux"
+                    />
+                    <img
+                      className={styles["tech-icons"]}
+                      src="/icons/react-stripe.svg"
+                      alt="React Stripe"
+                    />
+                    <img
+                      className={styles["tech-icons"]}
+                      src="/icons/react-tabulator.png"
+                      alt="React Tabulator"
+                    />
+                  </div>
+                </div>
+              </Col>
+              <Col className="my-4">
+                <h4>Back-end</h4>
+                <div className="row d-flex gap-3 justify-content-center">
+                  <img
+                    className={styles["tech-icons"]}
+                    src="/icons/express-js.svg"
+                    alt="Express"
+                  />
+                  <img
+                    className={styles["tech-icons"]}
+                    src="/icons/faker-js.svg"
+                    alt="Faker.js"
+                  />
+                  <img
+                    className={styles["tech-icons"]}
+                    src="/icons/node.svg"
+                    alt="Node.js"
+                  />
+                  <img
+                    className={styles["tech-icons"]}
+                    src="/icons/postman.svg"
+                    alt="Postman"
+                  />
 
-                    <img
-                      className={styles["tech-icons"]}
-                      src="/icons/sequelize.svg"
-                      alt="Sequelize"
-                    />
-                  </div>
-                </Col>
-              </Row>
-              <Row>
-                <Col className="my-4">
-                  <h4>Control de versiones</h4>
-                  <div className="row d-flex gap-3 justify-content-center">
-                    <img
-                      className={styles["tech-icons"]}
-                      src="/icons/git.svg"
-                      alt="Git"
-                    />
-                    <img
-                      className={styles["tech-icons"]}
-                      src="/icons/github.svg"
-                      alt="Github"
-                    />
-                  </div>
-                </Col>
-                <Col className="my-4">
-                  <h4>Base de datos</h4>
-                  <div className="row d-flex gap-3 justify-content-center">
-                    <img
-                      className={styles["tech-icons"]}
-                      src="/icons/mysql.svg"
-                      alt="MySQL"
-                    />
-                  </div>
-                </Col>
-              </Row>
-            </div>
+                  <img
+                    className={styles["tech-icons"]}
+                    src="/icons/sequelize.svg"
+                    alt="Sequelize"
+                  />
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="my-4">
+                <h4>Control de versiones</h4>
+                <div className="row d-flex gap-3 justify-content-center">
+                  <img
+                    className={styles["tech-icons"]}
+                    src="/icons/git.svg"
+                    alt="Git"
+                  />
+                  <img
+                    className={styles["tech-icons"]}
+                    src="/icons/github.svg"
+                    alt="Github"
+                  />
+                </div>
+              </Col>
+              <Col className="my-4">
+                <h4>Base de datos</h4>
+                <div className="row d-flex gap-3 justify-content-center">
+                  <img
+                    className={styles["tech-icons"]}
+                    src="/icons/mysql.svg"
+                    alt="MySQL"
+                  />
+                </div>
+              </Col>
+            </Row>
           </div>
         </Container>
       </div>
 
-      <div id="team" className="bg-light py-5">
-        <div className="container my-4">
-          <div className="row text-center py-3">
-            <div className="col-lg-6 m-auto">
-              <h2 className="text-center">Nuestro Equipo</h2>
-              <p>
-                Nuestro equipo está compuesto por profesionales apasionados y
-                dedicados que aportan una amplia gama de habilidades y
-                experiencia.
-              </p>
+      <div className="bg-light py-5">
+        <Container className="my-4">
+          <h2 className="text-center">Nuestro Equipo</h2>
+          <p className="text-center mb-4">
+            Nuestro equipo está compuesto por estudiantes apasionados y
+            dedicados que aportan una amplia gama de habilidades.
+          </p>
 
-              <div className="row">
-                <div className="col-md-3">
-                  <div className="text-center">
-                    <img
-                      src="https://i.pinimg.com/564x/54/08/39/5408399b4f0e4137961ef81ee61289c9.jpg"
-                      className={styles["img-fluid"]}
-                      alt=""
-                    />
-                    <p>Ignacio Castellan</p>
-                    <div className="d-flex flex-row justify-content-center gap-2">
-                      <i className="bi bi-linkedin"></i>
-                      <i className="bi bi-github"></i>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="col-md-3">
-                  <div className="text-center">
-                    <img
-                      src="https://i.pinimg.com/originals/80/38/ef/8038ef4599953301e18d739caf1b4d18.jpg"
-                      className={styles["img-fluid"]}
-                      alt=""
-                    />
-                    <p>Jean Franco Pisciottano</p>
-                    <div className="d-flex flex-row justify-content-center gap-2">
-                      <i className="bi bi-linkedin"></i>
-                      <i className="bi bi-github"></i>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="col-md-3">
-                  <div className="text-center">
-                    <img
-                      src="https://i.pinimg.com/originals/e4/8b/2f/e48b2f53314783acf8b12fbf8ce3fa8e.jpg"
-                      className={styles["img-fluid"]}
-                      alt=""
-                    />
-                    <p>Abril Pereira Diaz</p>
-                    <div className="d-flex flex-row justify-content-center gap-2">
-                      <i className="bi bi-linkedin"></i>
-                      <i className="bi bi-github"></i>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="col-md-3">
-                  <div className="text-center">
-                    <img
-                      src="https://i.pinimg.com/originals/ff/a1/56/ffa1564f87643dcb905707ecc3aed565.jpg"
-                      className={styles["img-fluid"]}
-                      alt=""
-                    />
-                    <p>Agustin Acosta</p>
-                    <div className="d-flex flex-row justify-content-center gap-2">
-                      <i className="bi bi-linkedin"></i>
-                      <i className="bi bi-github"></i>
-                    </div>
-                  </div>
-                </div>
+          <Row className={`${styles["members-box"]}`}>
+            <div className={styles["members"]}>
+              <img
+                src="https://i.pinimg.com/564x/54/08/39/5408399b4f0e4137961ef81ee61289c9.jpg"
+                className={styles["img-fluid"]}
+                alt=""
+              />
+              <p className="mt-2 mb-1">Ignacio Castellan</p>
+              <div className="d-flex flex-row justify-content-center gap-2">
+                <a className={styles["member-accounts"]} href="">
+                  <i className="bi bi-linkedin"></i>
+                </a>
+                <a className={styles["member-accounts"]} href="">
+                  <i className="bi bi-github"></i>
+                </a>
               </div>
             </div>
-          </div>
-        </div>
+
+            <div className={styles["members"]}>
+              <img
+                src="https://i.pinimg.com/originals/80/38/ef/8038ef4599953301e18d739caf1b4d18.jpg"
+                className={styles["img-fluid"]}
+                alt=""
+              />
+              <p className="mt-2 mb-1">Jean Franco Pisciottano</p>
+              <div className="d-flex flex-row justify-content-center gap-2">
+                <a className={styles["member-accounts"]} href="">
+                  <i className="bi bi-linkedin"></i>
+                </a>
+                <a className={styles["member-accounts"]} href="">
+                  <i className="bi bi-github"></i>
+                </a>
+              </div>
+            </div>
+
+            <div className={styles["members"]}>
+              <img
+                src="https://i.pinimg.com/originals/e4/8b/2f/e48b2f53314783acf8b12fbf8ce3fa8e.jpg"
+                className={styles["img-fluid"]}
+                alt=""
+              />
+              <p className="mt-2 mb-1">Abril Pereira Diaz</p>
+              <div className="d-flex flex-row justify-content-center gap-2">
+                <a className={styles["member-accounts"]} href="">
+                  <i className="bi bi-linkedin"></i>
+                </a>
+                <a className={styles["member-accounts"]} href="">
+                  <i className="bi bi-github"></i>
+                </a>
+              </div>
+            </div>
+
+            <div className={styles["members"]}>
+              <img
+                src="https://i.pinimg.com/originals/ff/a1/56/ffa1564f87643dcb905707ecc3aed565.jpg"
+                className={styles["img-fluid"]}
+                alt=""
+              />
+              <p className="mt-2 mb-1">Agustin Acosta</p>
+              <div className="d-flex flex-row justify-content-center gap-2">
+                <a className={styles["member-accounts"]} href="">
+                  <i className="bi bi-linkedin"></i>
+                </a>
+                <a className={styles["member-accounts"]} href="">
+                  <i className="bi bi-github"></i>
+                </a>
+              </div>
+            </div>
+          </Row>
+        </Container>
       </div>
 
       <Footer />
