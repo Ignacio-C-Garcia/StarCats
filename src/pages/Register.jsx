bcd846d6cd260857e9775669f1d8f486d367872b
Ichiro <130695643+ichirothecringy-dev@users.noreply.github.com>
2024-07-12 12:13:18 -0300

    login and register style finished

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,85 +1,94 @@
 import styles from "../styles/Login.module.css";
-import { useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
 import Footer from "../components/Footer";
-import { Link } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+import React, { useState } from "react";
+import { Container, Row, Col, Form, Alert } from "react-bootstrap";
+import { Link, Navigate } from "react-router-dom";
+import ButtonComponent from "../components/ButtonComponent";
+
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
   };
 
   return (
-    <>
-      <Container className="vh-100 d-flex justify-content-center">
-        <Row>
+    <div className={styles.login}>
+      <NavBar />
+      <Container className={styles.loginContainer}>
+        <Row className="h-100">
           <Col
-            xs={{ span: 12, order: "last" }}
-            md={{ span: 12, order: "last" }}
-            lg={{ span: 6, order: "first" }}
-            className="m-0 p-3 d-flex justify-content-center"
+            className="d-flex justify-content-center align-items-center"
+            lg={6}
           >
-            <Link to="/" className="d-flex align-items-center">
-              <img src="/logostarcats.svg" alt="starcat-logo" />
-            </Link>
+            <img
+              src="logostarcats.svg"
+              width={400}
+              height={400}
+              alt="StarCats Logo"
+            />
           </Col>
-          <Col className="m-0 p-3 d-flex flex-column justify-content-center">
-            <h2 className="text-center mb-3 fs-1">Regístrate</h2>
-            <div className={`rounded-4 p-4 w-100 ${styles.form}`}>
-              -- Volver en el futuro para editar los valores que correspondan al
-              back --
-              <form
+          <Col className={styles.formColumn}>
+            <div className="w-75">
+              <h2 className="text-center pb-2">Registrate</h2>
+              {/* {showAlert && (
+                <Alert
+                  variant="warning"
+                  className={styles.customAlert}
+                  onClose={() => setShowAlert(false)}
+                  dismissible
+                >
+                  Por favor, completa ambos campos.
+                  <button onClick={() => setShowAlert(false)}>×</button>
+                </Alert>
+              )} */}
+              <Form
                 onSubmit={handleSubmit}
-                className="d-flex align-items-center pb-2"
+                className="d-flex flex-column gap-2"
               >
-                <div className="w-100">
-                  <div>
-                    <input
-                      className="mb-2 p-2 border-0 rounded-pill w-100 text-center"
-                      placeholder="Nombre"
-                      type="text"
-                      value={name}
-                      onChange={(e) => setPassword(e.target.value)}
-                      required
-                    />
-                  </div>
-                  <div>
-                    <input
-                      className="mb-2 p-2 border-0 rounded-pill w-100 text-center"
-                      placeholder="Correo electrónico"
-                      type="email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      required
-                    />
-                  </div>
-                  <div>
-                    <input
-                      className="mb-2 p-2 border-0 rounded-pill w-100 text-center"
-                      placeholder="Contraseña"
-                      type="password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      required
-                    />
-                  </div>
-                  <button
-                    type="submit"
-                    className="p-2 border-0 rounded-pill w-100"
-                  >
-                    Crear cuenta
-                  </button>
-                </div>
-              </form>
+                <Form.Control
+                  type="text"
+                  placeholder="Ingresá tu nombre"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className={`${styles.inputForm} rounded-pill p-2 ps-3 pe-3`}
+                />
+                <Form.Control
+                  type="text"
+                  placeholder="Ingresá tu apellido"
+                  value={surname}
+                  onChange={(e) => setSurname(e.target.value)}
+                  className={`${styles.inputForm} rounded-pill p-2 ps-3 pe-3`}
+                />
+                <Form.Control
+                  type="email"
+                  placeholder="Ingresá un correo electrónico"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className={`${styles.inputForm} rounded-pill p-2 ps-3 pe-3`}
+                />
+                <Form.Control
+                  type="password"
+                  placeholder="Ingresá una contraseña"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className={`${styles.inputForm} rounded-pill p-2 ps-3 pe-3`}
+                />
+                <ButtonComponent type="submit" className="rounded-pill">
+                  Registrarse
+                </ButtonComponent>
+              </Form>
             </div>
           </Col>
         </Row>
       </Container>
       <Footer />
-    </>
+    </div>
   );
 };
 
